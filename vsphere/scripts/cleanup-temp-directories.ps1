6796ba2be0ca5a110a12ba0d813c3126d2547e28
Amin Jamali <ajamali@pivotal.io>
2017-01-17 16:03:52 -0500

    WIP- use vmware-tools in C:

--- vsphere/scripts/cleanup-temp-directories.ps1
+++ vsphere/scripts/cleanup-temp-directories.ps1
@@ -2,3 +2,6 @@ Get-ChildItem -Path C:\Windows\Temp |
     Select-Object -expandproperty fullname |
     Where { $_ -notlike "C:\Windows\Temp\vm*" } |
     Remove-Item -Force -Recurse
+
+Remove-Item -Path C:\*.exe -Force
+Remove-Item -Path C:\*.zip -Force
