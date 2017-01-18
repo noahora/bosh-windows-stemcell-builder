9cc0aaeccf0b3503348107668752040a30bf3248
Amin Jamali <ajamali@pivotal.io>
2017-01-18 07:09:00 -0500

    WIP-Move Cleanup after compact

--- vsphere/scripts/cleanup-temp-directories.ps1
+++ vsphere/scripts/cleanup-temp-directories.ps1
@@ -2,6 +2,3 @@ Get-ChildItem -Path C:\Windows\Temp |
     Select-Object -expandproperty fullname |
     Where { $_ -notlike "C:\Windows\Temp\vm*" } |
     Remove-Item -Force -Recurse
-
-Remove-Item -Path C:\*.exe -Force
-Remove-Item -Path C:\*.zip -Force
