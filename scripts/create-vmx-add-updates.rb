13118ec6f68effe4d8ad612d464bf2e64012defa
Natalie Arellano <narellano@pivotal.io>
2017-03-01 09:27:16 -0500

    fix path of vmx tarball location
    Signed-off-by: David Jahn <djahn@pivotal.io>

--- scripts/create-vmx-add-updates.rb
+++ scripts/create-vmx-add-updates.rb
@@ -116,10 +116,9 @@ begin
   exec_command("tar czvf #{new_vmx_tarball} -C #{OUTPUT_DIR} *")
   S3Client.new().Put(VMX_BUCKET, File.basename(new_vmx_tarball), new_vmx_tarball)
 
-  source_tarball = File.join(OUTPUT_DIR, new_vmx_tarball)
-  dest_tarball = File.join(VMX_CACHE, new_vmx_tarball)
-  puts "moving VMX tarball #{source_tarball} to #{dest_tarball}"
-  FileUtils.mv("#{source_tarball}", "#{dest_tarball}")
+  cached_tarball = File.join(VMX_CACHE, new_vmx_tarball)
+  puts "moving VMX tarball #{new_vmx_tarball} to #{cached_tarball}"
+  FileUtils.mv("#{new_vmx_tarball}", "#{cached_tarball}")
   puts "moving dir (#{OUTPUT_DIR}) to (#{new_dirname})"
   FileUtils.mv(OUTPUT_DIR, new_dirname)
 end
