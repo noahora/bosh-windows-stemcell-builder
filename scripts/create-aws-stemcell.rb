33a150180d925f9735859551c20812951b3da8a3
Paul Nikonowicz <pnikonowicz@pivotal.io>
2017-03-09 07:52:26 -0500

    use path for concourse tasks
    Signed-off-by: David Jahn <djahn@pivotal.io>

--- scripts/create-aws-stemcell.rb
+++ scripts/create-aws-stemcell.rb
@@ -3,12 +3,6 @@
 require_relative 'exec-command'
 require 'fileutils'
 
-FileUtils.mkdir_p(File.join("stemcell-builder","build"))
-FileUtils.cp_r("windows-stemcell-dependencies", File.join("stemcell-builder","build","windows-stemcell-dependencies"))
-FileUtils.cp_r("compiled-agent", File.join("stemcell-builder","build","compiled-agent"))
-FileUtils.cp_r("version", File.join("stemcell-builder","build","version"))
-FileUtils.cp_r("base-amis", File.join("stemcell-builder","build","base-amis"))
-
 Dir.chdir "stemcell-builder" do
   exec_command("bundle install")
   exec_command("rake build:aws")
--- scripts/create-gcp-stemcell.rb
+++ scripts/create-gcp-stemcell.rb
@@ -3,12 +3,6 @@
 require_relative 'exec-command'
 require 'fileutils'
 
-FileUtils.mkdir_p(File.join("stemcell-builder","build"))
-FileUtils.cp_r("windows-stemcell-dependencies", File.join("stemcell-builder","build","windows-stemcell-dependencies"))
-FileUtils.cp_r("compiled-agent", File.join("stemcell-builder","build","compiled-agent"))
-FileUtils.cp_r("base-gcp-image", File.join("stemcell-builder","build","base-gcp-image"))
-FileUtils.cp_r("version", File.join("stemcell-builder","build","version"))
-
 Dir.chdir "stemcell-builder" do
   exec_command("bundle install")
   exec_command("rake build:gcp")
