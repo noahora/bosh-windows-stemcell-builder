f556c4b2e18c0ae1431f1c2b2e042e086d503378
Paul Nikonowicz <pnikonowicz@pivotal.io>
2017-03-31 18:22:20 -0400

    WIP sysprep cmdlet/module
    [#142852453]
    
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

--- bosh-psmodules/modules/BOSH.Sysprep/BOSH.Sysprep.Tests.ps1
+++ bosh-psmodules/modules/BOSH.Sysprep/BOSH.Sysprep.Tests.ps1
@@ -34,5 +34,62 @@ Describe "Enable-LocalSecurityPolicy" {
     }
 }
 
+Describe "Create-Unattend" {
+    BeforeEach {
+        $UnattendDestination="." #(New-TempDir)
+        $NewPassword="NewPassword"
+        $ProductKey= "ProductKey"
+        $Organization="Organization"
+        $Owner="Owner"
+        {
+            Create-Unattend -UnattendDestination $UnattendDestination `
+                -NewPassword $NewPassword `
+                -ProductKey $ProductKey `
+                -Organization $Organization `
+                -Owner $Owner
+        } | Should Not Throw
+    }
+
+    AfterEach {
+        #Remove-Item -Recurse -Force $UnattendDestination
+    }
+
+    It "places the generated Unattend file in the specified directory" {
+        Test-Path (Join-Path $UnattendDestination "unattend.xml") | Should Be $True
+    }
+
+    Context "the generated Unattend file" {
+        BeforeEach {
+            $FullPath = (Join-Path $UnattendDestination "unattend.xml")
+            [xml]$unattendXML = Get-Content -Path $FullPath
+        }
+
+        It "is valid xml" {}
+
+        It "contains a New Password" {
+            $expected = $NewPassword
+            Write-Host $unattendXML.unattend.ToString().Count
+            Write-Host "BYE"
+            $unattendXML.unattend.settings.component.UserAccounts.AdministratorPassword.Value | Should Be $expected
+        }
+
+        It "Product Key" {
+            $expected= $ProductKey
+            Write-Host $unattendXML.unattend.settings.component.ProductKey.InnerXML
+            #$unattendXML.unattend.settings.component.ProductKey | Should Be $expected
+        }
+
+        It "Organization" {
+            $expected= $Organization
+            { $unattendXML.unattend.settings.component.RegisteredOrganization } | Should Be $expected
+        }
+
+        It "Owner" {
+            $expected= $Owner
+            $unattendXML.unattend.settings.component.RegisteredOwner | Should Be $expected
+        }
+    }
+}
+
 Remove-Module -Name BOSH.Sysprep -ErrorAction Ignore
 Remove-Module -Name BOSH.Utils -ErrorAction Ignore
